b80a8357d9f4f359acf73bf29be26ff21384eb1e
sarindy <sarindyo@yahoo.com>
2014-07-21 14:48:37 +0700

    Circles

--- src/abstract_factory_pattern/AbstractFactoryPatternDemo.java
+++ src/abstract_factory_pattern/AbstractFactoryPatternDemo.java
@@ -5,7 +5,7 @@ public class AbstractFactoryPatternDemo {
 	public static void main(String[] args) {
 		AbstractFactory shapeFactory=FactoryProducer.getFactory("shape");
 		
-		Shape shape1=shapeFactory.getShape("circle");
+		Shape shape1=shapeFactory.getShape("circles");
 		shape1.draw();
 		
 		AbstractFactory colorFactory=FactoryProducer.getFactory("color");
